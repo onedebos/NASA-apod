58efdcf5bb5a6a5383eba240118f3bd6df056cce
Adebola <onedebos@gmail.com>
2020-07-24 01:50:19 +0100

    start project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Adebola's CRA template
+
+This is a React app bootstrapped with CRA Typescript and tailwindcss.
+
+## Getting Started
+
+To use this template
+
+- `git clone git@github.com:onedebos/adebola-tailwindcss-react.git`
+- `yarn install`
---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import usersSliceReducer from "./features/user/UsersSlice";
+import * as serviceWorker from "./serviceWorker";
+
+const store = configureStore({
+  reducer: {
+    users: usersSliceReducer,
+    // obj: ojSlice.reducer
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App message={"Adebola's react + ts + tailwind starter pack."} />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
