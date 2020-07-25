ba3a0ca3e203c0f1b2f8d1e0ecab05696558dac2
Adebola <onedebos@gmail.com>
2020-07-25 17:16:05 +0100

    remove browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { photoSelector } from "./features/photo/PhotoSlice";
 import Photo from "./Photo";
@@ -8,12 +7,10 @@ import Footer from "./Footer";
 const App: React.FC = () => {
   const { loading } = useSelector(photoSelector);
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Photo} />
-      </Switch>
+    <div>
+      <Photo />
       {loading ? null : <Footer />}
-    </BrowserRouter>
+    </div>
   );
 };
 
