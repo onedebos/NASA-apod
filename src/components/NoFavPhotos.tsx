b8d7a5b13122928dcb7a8f68b664fceb57ed6c21
Adebola <onedebos@gmail.com>
2020-08-12 11:36:01 +0100

    style no favoroties

--- src/components/NoFavPhotos.tsx
+++ src/components/NoFavPhotos.tsx
@@ -4,10 +4,9 @@ const NoFavPhotos: React.FC = () => {
   return (
     <section className="md:max-w-lg lg:max-w-4xl m-auto px-6 pb-6">
       <h1 className="font-bold text-2xl md:text-5xl">Your Favorites!</h1>
-      <h3 className="font-semibold text-gray-600">All your saved images.</h3>
-      <h3 className="font-semibold text-gray-800 text-2xl p-4 bg-gray-100 leading-none">
+      <h3 className="font-semibold text-gray-800 text-xl md:text-2xl p-4 bg-gray-100 leading-none mb-2">
         You currently have no saved images.
-        <p className="font-bold text-xl text-gray-600 leading-none">
+        <p className="font-bold md:text-xl text-gray-600 leading-none">
           Start adding some to see them here.
         </p>
       </h3>
