97639f8a5b24373e1c13c21fcdfc9d9df8bbb5b2
Adebola <onedebos@gmail.com>
2020-08-12 11:21:30 +0100

    fix leading

--- src/components/NoFavPhotos.tsx
+++ src/components/NoFavPhotos.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const NoFavPhotos: React.FC = () => {
   return (
     <section className="md:max-w-lg lg:max-w-4xl m-auto px-6 pb-6">
-      <h1 className="font-bold text-5xl">Your Favorites!</h1>
+      <h1 className="font-bold text-2xl md:text-5xl">Your Favorites!</h1>
       <h3 className="font-semibold text-gray-600">All your saved images.</h3>
-      <h3 className="font-semibold text-gray-800 text-2xl p-4 bg-gray-100">
+      <h3 className="font-semibold text-gray-800 text-2xl p-4 bg-gray-100 leading-none">
         You currently have no saved images.
-        <p className="font-bold text-xl text-gray-600">
+        <p className="font-bold text-xl text-gray-600 leading-none">
           Start adding some to see them here.
         </p>
       </h3>
